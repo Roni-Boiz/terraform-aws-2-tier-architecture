3f824892701f21d9090899e5d8f4cc1fb6baf5dc
Roni-Boiz <2019cs155@stu.ucsc.lk>
2024-09-26 00:17:10 +0530

    format terraform files

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 module "vpc" {
-    source          = "./modules/vpc"
-    region          = var.region
-    project_name    = var.project_name
-    vpc_cidr        = var.vpc_cidr
-    pub_sub_1a_cidr = var.pub_sub_1a_cidr
-    pub_sub_2b_cidr = var.pub_sub_2b_cidr
-    pri_sub_3a_cidr = var.pri_sub_3a_cidr
-    pri_sub_4b_cidr = var.pri_sub_4b_cidr
-    pri_sub_5a_cidr = var.pri_sub_5a_cidr
-    pri_sub_6b_cidr = var.pri_sub_6b_cidr
+  source          = "./modules/vpc"
+  region          = var.region
+  project_name    = var.project_name
+  vpc_cidr        = var.vpc_cidr
+  pub_sub_1a_cidr = var.pub_sub_1a_cidr
+  pub_sub_2b_cidr = var.pub_sub_2b_cidr
+  pri_sub_3a_cidr = var.pri_sub_3a_cidr
+  pri_sub_4b_cidr = var.pri_sub_4b_cidr
+  pri_sub_5a_cidr = var.pri_sub_5a_cidr
+  pri_sub_6b_cidr = var.pri_sub_6b_cidr
 }
 
 module "nat" {
