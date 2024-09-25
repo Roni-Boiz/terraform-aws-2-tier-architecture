bf79e3cc6700d40d8fb0e4e9105fb0a7c458ec89
Roni-Boiz <2019cs155@stu.ucsc.lk>
2024-09-26 00:13:31 +0530

    remove bucket policies

--- aws-prerequisite-resources.sh
+++ aws-prerequisite-resources.sh
@@ -11,25 +11,6 @@ if aws s3api head-bucket --bucket "$BUCKET_NAME" &>/dev/null; then
 else
   aws s3api create-bucket --bucket "$BUCKET_NAME" --region "$REGION" --create-bucket-configuration LocationConstraint="$REGION"
   aws s3api wait bucket-exists --bucket "$BUCKET_NAME"
-  aws s3api put-bucket-policy --bucket "$BUCKET_NAME" --policy '{
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Principal": "*",
-        "Action": [
-          "s3:DeleteBucket",
-          "s3:ListBucket",
-          "s3:DeleteObject",
-          "s3:ListBucketVersions"
-        ],
-        "Resource": [
-          "arn:aws:s3:::'"$BUCKET_NAME"'",
-          "arn:aws:s3:::'"$BUCKET_NAME"'/*"
-        ]
-      }
-    ]
-  }'
   aws s3api put-bucket-versioning --bucket "$BUCKET_NAME" --versioning-configuration Status=Enabled
   aws s3api put-bucket-encryption --bucket "$BUCKET_NAME" --server-side-encryption-configuration '{
     "Rules": [{
