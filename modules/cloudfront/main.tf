40afae29919d67be51755af96462aaa946611f55
Roni-Boiz <2019cs155@stu.ucsc.lk>
2024-10-09 17:16:23 +0530

    quick fix

--- modules/cloudfront/main.tf
+++ modules/cloudfront/main.tf
@@ -14,7 +14,7 @@ resource "aws_cloudfront_distribution" "my_distribution" {
     custom_origin_config {
       http_port              = 80
       https_port             = 443
-      origin_protocol_policy = "https-only"
+      origin_protocol_policy = "http-only"
       origin_ssl_protocols   = ["TLSv1.2"]
     }
   }
