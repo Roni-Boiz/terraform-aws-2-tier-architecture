b6c8dfb3ee403399ac11e5783cc1a12f3d5a0cc9
Roni-Boiz <2019cs155@stu.ucsc.lk>
2024-10-09 10:36:56 +0530

    quick fix

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -35,33 +35,45 @@ resource "aws_lb_target_group" "alb_target_group" {
   }
 }
 
-# create a listener on port 443 with forward action
-resource "aws_lb_listener" "alb_https_listener" {
-  load_balancer_arn = aws_lb.application_load_balancer.arn
-  port              = 443
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  # certificate_arn   = var.acm_certificate_arn
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.alb_target_group.arn
-  }
-}
-
-# create a listener on port 80 with redirect action
+# create a listener on port 80 with forward action
 resource "aws_lb_listener" "alb_http_listener" {
   load_balancer_arn = aws_lb.application_load_balancer.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
-    type = "redirect"
-
-    redirect {
-      protocol    = "HTTPS"
-      port        = "443"
-      status_code = "HTTP_301"
-    }
+    type = "forward"
+    target_group_arn = aws_lb_target_group.alb_target_group.arn
   }
 }
+
+# # create a listener on port 443 with forward action
+# resource "aws_lb_listener" "alb_https_listener" {
+#   load_balancer_arn = aws_lb.application_load_balancer.arn
+#   port              = 443
+#   protocol          = "HTTPS"
+#   ssl_policy        = "ELBSecurityPolicy-2016-08"
+#   # certificate_arn   = var.acm_certificate_arn
+
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.alb_target_group.arn
+#   }
+# }
+
+# # create a listener on port 80 with redirect action
+# resource "aws_lb_listener" "alb_http_listener" {
+#   load_balancer_arn = aws_lb.application_load_balancer.arn
+#   port              = 80
+#   protocol          = "HTTP"
+
+#   default_action {
+#     type = "redirect"
+
+#     redirect {
+#       protocol    = "HTTPS"
+#       port        = "443"
+#       status_code = "HTTP_301"
+#     }
+#   }
+# }
