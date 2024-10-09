8a27de36b57bc0b027e51dc4892ebcdf9d7f03c8
Roni-Boiz <2019cs155@stu.ucsc.lk>
2024-10-09 10:42:51 +0530

    quick fix

--- modules/cloudfront/main.tf
+++ modules/cloudfront/main.tf
@@ -14,7 +14,7 @@ resource "aws_cloudfront_distribution" "my_distribution" {
     custom_origin_config {
       http_port              = 80
       https_port             = 443
-      origin_protocol_policy = "https-only"
+      origin_protocol_policy = "http-only"
       origin_ssl_protocols   = ["TLSv1.2"]
     }
   }
@@ -23,7 +23,7 @@ resource "aws_cloudfront_distribution" "my_distribution" {
     allowed_methods        = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
     cached_methods         = ["GET", "HEAD", "OPTIONS"]
     target_origin_id       = var.alb_domain_name
-    viewer_protocol_policy = "redirect-to-https"
+    viewer_protocol_policy = "redirect-to-http"
     forwarded_values {
       headers      = ["Host", "X-Forwarded-Proto", "Authorization"]
       query_string = true
